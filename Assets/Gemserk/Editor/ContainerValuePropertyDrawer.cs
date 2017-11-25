3d84c45d1e4944760abf37814b380f506aae403f
Ariel Coppes <ariel.coppes@gmail.com>
2017-11-24 22:15:40 -0300

    work on the value selector property drawer

--- Assets/Gemserk/Editor/ContainerValuePropertyDrawer.cs
+++ Assets/Gemserk/Editor/ContainerValuePropertyDrawer.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 using UnityEditor;
 using Gemserk.Values;
 
+using System.Linq;
+
 [CustomPropertyDrawer(typeof(SerializableContainerValue))]
 public class ContainerValuePropertyDrawer : PropertyDrawer
 {
 	const float propertyHeight = 16;
 
-	int totalFields = 1;
+	int totalFields = 2;
 
 	// Draw the property inside the given rect
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
@@ -21,16 +23,39 @@ public class ContainerValuePropertyDrawer : PropertyDrawer
 
 		var containerRect = new Rect(position.x, position.y + propertyHeight * 0, maxWidth, propertyHeight);
 
-		EditorGUI.ObjectField (containerRect, containerProperty, typeof(ValueContainerBehaviour));
+		containerProperty.objectReferenceValue = EditorGUI.ObjectField (containerRect, containerProperty.objectReferenceValue, typeof(ValueContainerBehaviour), true);
 
-		if (containerProperty == null) {
-			totalFields = 1;
+		var valueContainer = containerProperty.objectReferenceValue as ValueContainerBehaviour;
+
+		var keyRect = new Rect(position.x, position.y + propertyHeight * 1, maxWidth, propertyHeight);
+
+		if (valueContainer == null) {
+//			totalFields = 2;
+//			EditorGUI.LabelField (keyRect, "Select container");
 		} else {
-			totalFields = 2;
-			var keyRect = new Rect(position.x, position.y + propertyHeight * 1, maxWidth, propertyHeight);
+//			totalFields = 2;
 
-			// TODO: show select from all defined variables in container
-			keyProperty.stringValue = EditorGUI.TextField(keyRect, keyProperty.stringValue);
+			if (valueContainer.values.Count == 0) {
+				EditorGUI.LabelField (keyRect, "Empty Container");
+			} else {
+				var options = valueContainer.values.Select (v => v.name).ToList ();
+				options.Add ("None");
+
+				int currentSelection = options.IndexOf (keyProperty.stringValue);
+
+				if (currentSelection == -1)
+					currentSelection = options.Count - 1;
+
+				int newSelection = EditorGUI.Popup(keyRect, currentSelection, options.ToArray());
+
+				if (newSelection != currentSelection) {
+					if (newSelection == options.Count - 1)
+						keyProperty.stringValue = "";
+					else
+						keyProperty.stringValue = options [newSelection];
+				}
+			}
+
 		}
 
 		EditorGUI.EndProperty ();
